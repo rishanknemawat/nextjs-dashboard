81dba62039fcf843a2df6ab0cd685d99afd7fd00
nemawat.rakesh <nemawat.rakesh@ringcentral.com>
2023-12-19 12:49:21 +0530

    [4] Chapter 7 done

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import styles from './home.module.css';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { lusitana } from './ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
